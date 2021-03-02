d043bbb526b9f893c79113de19d9649358b15233
rafaelmm16 <rafael.merlomendes@gmail.com>
2021-03-02 14:48:44 -0300

    Modificações feitas no mEP2.

--- mEP2/mEP2.c
+++ mEP2/mEP2.c
@@ -3,32 +3,22 @@
 
 int main()
 {
-
-    int x = 0;
+    int x0 = 0, x1 = 0;
     int i = 0, j = 0;
     int ordem = 0;
-    int matrizA[ordem][ordem];
     int **matrizB;
     int matrizC[ordem][ordem];
-    int VetPerm[ordem];
+    int *VetPerm;
 
     scanf("%d", &ordem);
 
+    //Alocação do vetor de permutação
+    VetPerm = malloc(ordem * sizeof(int));
     //vetor de permutação
     for (i = 0; i < ordem; i++)
     {
         scanf("%d ", &VetPerm[i]);
     }
-
-    /*matriz A
-    for (i = 0; i < ordem; i++)
-    {
-        for (j = 0; j < ordem; j++)
-        {
-            scanf("%d", &matrizA[i][j]);
-        }
-    }
-    */
 
     //matriz B
     matrizB = malloc(ordem * sizeof(int *));
@@ -39,6 +29,96 @@ int main()
         scanf("%d ", matrizB[i]);
     }
 
+    //Matriz de Permutação
+    for (i = 0; i < ordem; i++)
+    {
+        x0 = 0;
+        x1 = 0;
+        for (j = 0; j < ordem; j++)
+        {
+            if (matrizB[i][j] == 0)
+            {
+                x0++;
+
+                //Calculo da matriz C
+                for (i = 0; i < ordem; i++)
+                {
+                    for (j = 0; j < ordem; j++)
+                    {
+                        matrizC[i][j] = matrizB[i][j] * VetPerm[i];
+                    }
+                }
+            }
+            else if (matrizB[i][j] == 1)
+            {
+                x1++;
+
+                //Calculo da matriz C
+                for (i = 0; i < ordem; i++)
+                {
+                    for (j = 0; j < ordem; j++)
+                    {
+                        matrizC[i][j] = matrizB[i][j] * VetPerm[i];
+                    }
+                }
+            }
+        }
+    }
+
+    for (j = 0; j < ordem; j++)
+    {
+        x0 = 0;
+        x1 = 0;
+        for (i = 0; i < ordem; i++)
+        {
+            if (matrizB[i][j] == 0)
+            {
+                x0++;
+                //Calculo da matriz C
+                for (i = 0; i < ordem; i++)
+                {
+                    for (j = 0; j < ordem; j++)
+                    {
+                        matrizC[i][j] = matrizB[i][j] * VetPerm[i];
+                    }
+                }
+            }
+            else
+            {
+                if (matrizB[i][j] == 1)
+                {
+                    x1++;
+                    //Calculo da matriz C
+                    for (i = 0; i < ordem; i++)
+                    {
+                        for (j = 0; j < ordem; j++)
+                        {
+                            matrizC[i][j] = matrizB[i][j] * VetPerm[i];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //printf("A matriz e de permutacao\n");
+
+    //Impressão da matriz C
+    for (i = 0; i < ordem; i++)
+    {
+        for (j = 0; j < ordem; j++)
+        {
+            printf("%d ", matrizC[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < ordem; i++)
+    {
+        free(matrizB[i]);
+        matrizB[i] = NULL;
+    }
     free(matrizB);
+    matrizB = NULL;
     return 0;
 }
