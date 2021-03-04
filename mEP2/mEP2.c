d3cdd14a02161a13a679fa15d877b8215c8d5833
rafaelmm16 <rafael.merlomendes@gmail.com>
2021-03-04 18:04:42 -0300

    Alterações no codigo.

--- mEP2/mEP2.c
+++ mEP2/mEP2.c
@@ -1,43 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Funcao testa se e permutacao//
+
+int permutacao(int **mat, int i, int j, int somai[], int somaj[])
+
+{
+
+    int x, y, per = 1;
+
+    for (x = 0; x < i; x++)
+
+        if (somai[x] != 1 || somaj[x] != 1)
+
+            per = 0;
+
+    return (per);
+}
+
 int main()
 {
     int i = 0, j = 0;
     int ordem = 0;
     int **matrizB;
     int matrizC[ordem][ordem];
+    int matrizA[ordem][ordem];
     int *VetPerm;
+    int somaprod = 0;
 
+    printf("ordem:\n");
     scanf("%d", &ordem);
 
     //Alocação do vetor de permutação
     VetPerm = malloc(ordem * sizeof(int));
-    //vetor de permutação
-    for (i = 0; i < ordem; i++)
+    if (VetPerm == NULL)
     {
-        scanf("%d ", &VetPerm[i]);
+        printf("Sem memória \n");
+        exit(1);
     }
 
-    //matriz B
+    //vetor de permutação
+    printf("permutacao\n");
+    for (i = 0; i < ordem - 1; i++)
+    {
+        scanf("%d ", &VetPerm[i]);
+        //printf("[%d]=%d\n", i, ordem);
+    }
+
+    //Alocação da matriz B
     matrizB = malloc(ordem * sizeof(int *));
     for (i = 0; i < ordem; i++)
     {
         matrizB[i] = malloc(ordem * sizeof(int));
+    }
 
-        scanf("%d ", matrizB[i]);
+    //Leitura da matriz B
+    printf("matriz B:\n");
+    for (i = 0; i < ordem; i++)
+    {
+        for (j = 0; j < ordem; j++)
+        {
+            scanf("%d ", matrizB[i]);
+        }
     }
 
     //Matriz de Permutação
 
     //Calculo da matriz C
+    /*
     for (i = 0; i < ordem; i++)
     {
         for (j = 0; j < ordem; j++)
         {
-            matrizC[i][j] = matrizB[i][j] * VetPerm[i];
+            somaprod = 0;
+            for (i = 0; i < ordem; i++)
+            {
+                somaprod += matrizA[i][i] * matrizB[i][j];
+                matrizC[i][j] = somaprod;
+            }
+            //matrizC[i][j] = *matrizB[i] * VetPerm[i];
         }
     }
+    */
 
     //Impressão da matriz C
     for (i = 0; i < ordem; i++)
@@ -48,6 +92,8 @@ int main()
         }
         printf("\n");
     }
+
+    //permutacao(matrizB, i, j, matrizA, matrizC);
 
     for (i = 0; i < ordem; i++)
     {
