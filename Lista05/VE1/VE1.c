1f87d58b554be462b8d4c6407393a78d301c9463
rafaelmm16 <rafael.merlomendes@gmail.com>
2021-03-14 16:08:10 -0300

    Lista 05, VE1, completo.

--- Lista05/VE1/VE1.c
+++ Lista05/VE1/VE1.c
@@ -15,29 +15,38 @@ VE1 *criaVE1(int capacidade)
     /*Verifique se o valor em capacidade é válido */
     /*Se for, inicialize n com -1 (indicando que a estrutura está vazia) */
     /*Aloque o array 'P' e retorne o ponteiro da estrutura */
+    if (capacidade < 1)
+    {
+        return NULL;
+    }
 
     struct ve1 *VE1;
 
-    /*if (VE1 == NULL)
+    VE1 = malloc(sizeof(VE1));
+    if (VE1 == NULL)
     {
-        printf("Sem memoria");
+        printf("Sem memoria\n");
         exit(1);
     }
-    else
-    {*/
-    VE1 = malloc(sizeof(int));
+
+    VE1->capacidade = capacidade;
 
     VE1->n = -1;
-    //}
+
+    VE1->P = malloc(capacidade * sizeof(int));
+
     if (VE1->P == NULL)
     {
-        printf("Sem memoria");
+        free(VE1);
+        printf("Sem memoria\n");
         exit(1);
     }
     else
     {
-        VE1->P = (int *)malloc(capacidade * sizeof(int));
-        printf("Chega\n");
+        for (int i = 0; i < capacidade; i++)
+        {
+            VE1->P[i] = -1;
+        }
     }
 
     return VE1;
@@ -57,71 +66,84 @@ void adicionaVE1(VE1 *ve, int v)
 {
     /* Adiciona um elemento no fim do vetor */
     /* Lembre-se de verificar se há espaço para adicionar o elemento */
-    /* Complexidade: O(???)  */
+    /* Complexidade: O(n)  */
 
-    int i = 0;
-    struct ve1 *v1;
-
-    v1 = malloc(sizeof(int));
-
-    for (i = 0; i > v1->capacidade; i--)
+    if (ve == NULL || ve->P == NULL)
     {
-        if (ve < v1->capacidade)
-        {
-            ve[i] = ve[v];
-            printf("Preencheu %d\n", ve[i]);
-        }
+        printf("Sem memoria\n");
+        return;
+    }
+    if (ve->n < ve->capacidade - 1)
+    {
+        ve->n++;
+        ve->P[ve->n] = v;
+    }
+    else
+    {
+        printf("Sem espaco\n");
     }
 }
 
 void removeVE1(VE1 *ve)
 {
     /* Retira o ultimo elemento*/
-    /* Complexidade: O(???)  */
+    /* Complexidade: O(n)  */
 
-    int i = 0;
-    struct ve1 *v1;
-
-    v1 = malloc(sizeof(int));
-
-    for (i = 0; i > v1->capacidade; i--)
+    if (ve->n < 0)
     {
-        if (ve < v1->capacidade)
-        {
-            ve[i] = ve[i + 1];
-            printf("Removeu %d\n", ve[i]);
-        }
+        return;
     }
+    ve->P[ve->n] = -1;
+    ve->n--;
 }
 
 int obtemElementoVE1(VE1 *ve)
 {
     /*Retorna o elemento ultimo */
-    /* Complexidade: O(???)  */
+    /* Complexidade: O(1)  */
 
-    return -1;
+    return ve->P[ve->n];
 }
 
 int vazioVE1(VE1 *ve)
 {
     /* Verifica se a estrutura possui algum elemento ou está vazia */
-    /* Complexidade: O(???)  */
+    /* Complexidade: O(1)  */
 
-    return -1;
+    if (ve->P[0] >= 0)
+    {
+        return 0;
+    }
+
+    return 1;
 }
 
 int cheioVE1(VE1 *ve)
 {
     /* Verifica se a estrutura está cheia */
-    /* Complexidade: O(???)  */
+    /* Complexidade: O(n)  */
 
-    return -1;
+    if (ve->capacidade == ve->n + 1)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 int tamanhoVE1(VE1 *ve)
 {
     /* Retorna o número de elementos que a estrutura armazena */
-    /* Complexidade: O(???)  */
+    /* Complexidade: O(n)  */
 
-    return -1;
+    int i = 0;
+    int soma = 0;
+
+    for (i = 0; i < ve->capacidade; i++)
+    {
+        if (ve->P[i] >= 0)
+        {
+            soma++;
+        }
+    }
+    return soma;
 }
